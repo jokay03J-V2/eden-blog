adffce8e601e5f6140cd26914db6e7d1077325ee
jokay03J-V2 <jmagro@edenschool.fr>
2023-03-15 10:22:47 +0100

    update database migration
    * create posts migrations

---
+++ database/migrations/1678459688043_posts.ts
@@ -0,0 +1,31 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'posts'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').notNullable()
+      table.json('image').nullable()
+      table.string('title', 50).notNullable()
+      table.string('content').notNullable()
+      table
+        .integer('author_id')
+        .unsigned()
+        .references('id')
+        .inTable('users')
+        .onDelete('CASCADE')
+        .notNullable()
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
